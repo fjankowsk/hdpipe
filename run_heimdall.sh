f361065bc48d4c3778e1d86442fa3f47e2dec447
Fabian Jankowski <fjankowsk@gmail.com>
2018-03-12 18:54:44 +0100

    Use GPU 0 for now.

--- run_heimdall.sh
+++ run_heimdall.sh
@@ -5,11 +5,11 @@
 #
 
 # frequency channels to zap
-# for Lovell telescope, 1400 MHz
+# for Lovell telescope, 1400 MHz, 672 frequency channels
 zap_list="-zap_chans 48 53 -zap_chans 191 193 -zap_chans 211 230 -zap_chans
 252 257 -zap_chans 284 340 -zap_chans 361 365 -zap_chans 409 410 -zap_chans
 416 420 -zap_chans 447 451 -zap_chans 461 468 -zap_chans 472 476 -zap_chans
-480 484 -zap_chans 668 683 -zap_chans 720 725 -zap_chans 731 734"
+480 484 -zap_chans 668 671"
 
 fils=(*.fil)
 echo "Number of files to process: ${#fils[@]}"
@@ -18,5 +18,6 @@ sleep 3
 for file in ${fils[@]}; do
     echo "Processing: ${file}"
     heimdall -dm 0 2000 -dm_tol 1.05 ${zap_list} \
+    -gpu_id 0 \
     -f ${file}
 done

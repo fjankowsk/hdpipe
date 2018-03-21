afccc188344e7f199edac8faf89dddad23603c63
Fabian Jankowski <fjankowsk@gmail.com>
2018-03-21 16:49:40 +0000

    Add the additional zapped channels. Heimdall will ignore them if the are not present in the data. We need something else for 80cm data though.

--- run_heimdall.sh
+++ run_heimdall.sh
@@ -5,11 +5,8 @@
 #
 
 # frequency channels to zap
-# for Lovell telescope, 1400 MHz, 672 frequency channels
-zap_list="-zap_chans 48 53 -zap_chans 191 193 -zap_chans 211 230 -zap_chans
-252 257 -zap_chans 284 340 -zap_chans 361 365 -zap_chans 409 410 -zap_chans
-416 420 -zap_chans 447 451 -zap_chans 461 468 -zap_chans 472 476 -zap_chans
-480 484 -zap_chans 668 671"
+# for Lovell telescope, 1400 MHz, 672 and 800 frequency channels
+zap_list="-zap_chans 48 53 -zap_chans 191 193 -zap_chans 211 230 -zap_chans 252 257 -zap_chans 284 340 -zap_chans 361 365 -zap_chans 409 410 -zap_chans 416 420 -zap_chans 447 451 -zap_chans 461 468 -zap_chans 472 476 -zap_chans 480 484 -zap_chans 668 671 -zap_chans 672 683 -zap_chans 720 725 -zap_chans 731 734"
 
 fils=(*.fil)
 echo "Number of files to process: ${#fils[@]}"

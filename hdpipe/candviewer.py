62845859bd282fa4eb1207320981e60f910dd343
Fabian Jankowski <fjankowsk@gmail.com>
2019-10-11 18:21:45 +0100

    Remove shebang.

--- hdpipe/candviewer.py
+++ hdpipe/candviewer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #   Visualize heimdall single-pulse candidates.
--- hdpipe/run_heimdall.py
+++ hdpipe/run_heimdall.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #   Run heimdall single-pulse detection pipeline.

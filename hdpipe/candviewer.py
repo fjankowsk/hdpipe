f77f2676320b2df7dffde887d39f0ab86f0d811d
Fabian Jankowski <fjankowsk@gmail.com>
2020-05-14 14:21:55 +0100

    Improve formatting.

--- hdpipe/candviewer.py
+++ hdpipe/candviewer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #   Visualize heimdall single-pulse candidates.
-#   2018-2019 Fabian Jankowski
+#   2018 - 2020 Fabian Jankowski
 #
 
 import argparse
@@ -21,6 +21,68 @@ import numpy as np
 
 from hdpipe.general_helpers import (signal_handler, setup_logging)
 from hdpipe.version import __version__
+
+
+def parse_args():
+    """
+    Parse the commandline arguments.
+
+    Returns
+    -------
+    args: populated namespace
+        The commandline arguments.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="View heimdall candidates.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "candfiles",
+        type=str,
+        nargs="+",
+        help="Candidate files to process."
+    )
+
+    parser.add_argument(
+        "-o", "--output",
+        action="store_true",
+        dest="output",
+        default=False,
+        help="Output plots to file rather than to screen."
+    )
+
+    parser.add_argument(
+        "-z", "--zap_mode",
+        dest="zap_mode",
+        type=str,
+        choices=[
+            "None",
+            "Lovell_20cm", "Lovell_80cm",
+            "MeerKAT_20cm"
+            ],
+        default="None",
+        help="Frequency zap mask mode to use."
+    )
+
+    parser.add_argument(
+        "-n", "--nchan",
+        type=int,
+        dest="nchan",
+        default=32,
+        help="Scrunch to this many frequency channels."
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=__version__
+    )
+
+    args = parser.parse_args()
+
+    return args
 
 
 def dtype_add_fields(data, dtype):
@@ -51,17 +113,19 @@ def load_data(filename):
     """
 
     dtype = [
-        ("snr","float"), ("samp_nr","int"), ("time","float"),
-        ("filter","int"),
-        ("dmtrial","int"), ("dm","float"),
-        ("n_clusters","int"), ("start","int"), ("end","int")
+        ("snr",float), ("samp_nr",int), ("time",float),
+        ("filter",int),
+        ("dmtrial",int), ("dm",float),
+        ("n_clusters",int), ("start",int), ("end",int)
         ]
 
     temp = np.genfromtxt(filename, dtype=dtype)
     temp = np.atleast_1d(temp)
 
-    dtype = [("cand_file","|U4096"), ("fil_file","|U4096"),
-             ("total_time","float")]
+    dtype = [
+        ("cand_file","|U4096"), ("fil_file","|U4096"),
+        ("total_time",float)
+    ]
     new_dtype = dtype_add_fields(temp, dtype)
 
     data = np.zeros(len(temp), dtype=new_dtype)
@@ -118,16 +182,27 @@ def plot_candidates(t_data, filename, output_plots):
 
     if len(data) > 0:
         for item in data:
-            print("{0}, {1}, {2}, {3}".format(item["snr"], item["dm"],
-                                              item["filter"], item["cand_file"]))
+            print("{0}, {1}, {2}, {3}".format(
+                item["snr"],
+                item["dm"],
+                item["filter"],
+                item["cand_file"]
+                )
+            )
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    sc = ax.scatter(data["dm"] + 1, data["snr"],
-                    c=data["filter"],
-                    marker="o")
-    plt.colorbar(sc, label="Filter number")
+    sc = ax.scatter(
+        data["dm"] + 1,
+        data["snr"],
+        c=data["filter"],
+        marker="o"
+    )
+    plt.colorbar(
+        sc,
+        label="Filter number"
+    )
 
     ax.set_xscale("log")
     ax.grid()
@@ -138,7 +213,10 @@ def plot_candidates(t_data, filename, output_plots):
     fig.tight_layout()
 
     if output_plots:
-        fig.savefig("{0}_snr_dm.png".format(os.path.basename(filename)[0:-5]), bbox_inches="tight")
+        fig.savefig(
+            "{0}_snr_dm.png".format(os.path.basename(filename)[0:-5]),
+            bbox_inches="tight"
+        )
 
         # close the figure in order not
         # to consume too much memory
@@ -166,21 +244,30 @@ def plot_clusters(t_data, filename, output_plots):
     fig = plt.figure()
     ax1 = fig.add_subplot(311)
 
-    ax1.scatter(data["dm"], data["n_clusters"])
+    ax1.scatter(
+        data["dm"],
+        data["n_clusters"]
+    )
     ax1.grid(True)
     ax1.set_yscale("log")
     ax1.set_xlabel('DM (pc/cm3)')
     ax1.set_ylabel('#clusters')
 
     ax2 = fig.add_subplot(312)
-    ax2.scatter(data["snr"], data["n_clusters"])
+    ax2.scatter(
+        data["snr"],
+        data["n_clusters"]
+    )
     ax2.grid(True)
     ax2.set_yscale("log")
     ax2.set_xlabel('S/N')
     ax2.set_ylabel('#clusters')
 
     ax3 = fig.add_subplot(313)
-    ax3.scatter(data["filter"], data["n_clusters"])
+    ax3.scatter(
+        data["filter"],
+        data["n_clusters"]
+    )
     ax3.grid(True)
     ax3.set_yscale("log")
     ax3.set_xlabel('Filter number')
@@ -189,7 +276,10 @@ def plot_clusters(t_data, filename, output_plots):
     fig.tight_layout()
 
     if output_plots:
-        fig.savefig("{0}_clusters.png".format(os.path.basename(filename)[0:-5]), bbox_inches="tight")
+        fig.savefig(
+            "{0}_clusters.png".format(os.path.basename(filename)[0:-5]),
+            bbox_inches="tight"
+        )
 
         # close the figure in order not
         # to consume too much memory
@@ -222,15 +312,21 @@ def plot_candidate_timeline(t_data, filename, output_plots):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    sc = ax.scatter(data["total_time"], data["dm"] + 1,
-                    c=2**data["filter"],
-                    norm=LogNorm(),
-                    s=60.0 * data["snr"] / np.max(data["snr"]),
-                    marker="o",
-                    edgecolor="black",
-                    lw=0.6,
-                    cmap="Reds")
-    plt.colorbar(sc, label="Filter number")
+    sc = ax.scatter(
+        data["total_time"],
+        data["dm"] + 1,
+        c=2**data["filter"],
+        norm=LogNorm(),
+        s=60.0 * data["snr"] / np.max(data["snr"]),
+        marker="o",
+        edgecolor="black",
+        lw=0.6,
+        cmap="Reds"
+    )
+    plt.colorbar(
+        sc,
+        label="Filter number"
+    )
 
     ax.grid()
     ax.set_xlabel("Time (s)")
@@ -241,7 +337,10 @@ def plot_candidate_timeline(t_data, filename, output_plots):
     fig.tight_layout()
 
     if output_plots:
-        fig.savefig("{0}_timeline.png".format(os.path.basename(filename)[0:-5]), bbox_inches="tight")
+        fig.savefig(
+            "{0}_timeline.png".format(os.path.basename(filename)[0:-5]),
+            bbox_inches="tight"
+        )
 
         # close the figure in order not
         # to consume too much memory
@@ -261,7 +360,7 @@ def get_zap_file(zap_mode):
     -------
     zap_file : str
         Absolute filename of zap file to use.
-    
+
     Raises
     ------
     RuntimeError
@@ -324,8 +423,8 @@ zap_mode, nchan=0, nbin=0, length=0):
         raise RuntimeError("Filterbank file does not exist: {0}".format(fil_file))
 
     # determine data parameters
-    cmd = "header {0} -tsamp -tobs -tstart -nchans -foff -fch1".format(fil_file)
-    args = shlex.split(cmd)
+    command = "header {0} -tsamp -tobs -tstart -nchans -foff -fch1".format(fil_file)
+    args = shlex.split(command)
     raw = subprocess.check_output(args, encoding="ascii")
     info = raw.split("\n")
 
@@ -337,36 +436,51 @@ zap_mode, nchan=0, nbin=0, length=0):
     foff = float(info[4].strip())
     fch1 = float(info[5].strip())
     rec_bw = float(abs(foff * rec_nchan))
-    rec_cfreq = float(fch1 + 0.5*rec_nchan * foff)
+    rec_cfreq = float(fch1 + 0.5 * rec_nchan * foff)
 
     cand_time = samp_time * sample
     # determine mjd of candidate
-    cand_mjd = float(tstart + cand_time/(60*60*24.0))
+    cand_mjd = float(tstart + cand_time / (60 * 60 * 24.0))
 
-    log.info("Data parameters: {0}, {1}, {2}, {3}, {4}, {5} MJD".format(samp_time, tobs,
-                                                                        rec_cfreq, rec_nchan,
-                                                                        rec_bw, tstart))
+    log.info("Data parameters: {0}, {1}, {2}, {3}, {4}, {5} MJD".format(
+        samp_time,
+        tobs,
+        rec_cfreq,
+        rec_nchan,
+        rec_bw,
+        tstart
+        )
+    )
     log.info("Candidate parameters: {0} s, {1} MJD".format(cand_time, cand_mjd))
 
     # use the absolute path here
     fil_file = os.path.abspath(fil_file)
 
-    cmd = "dmsmear -f {0} -b {1} -n {2} -d {3} -q".format(rec_cfreq,
-                                                          rec_bw, rec_nchan, dm)
-    args = shlex.split(cmd)
-    result = subprocess.check_output(args,
-                                     stderr=subprocess.STDOUT,
-                                     encoding="ascii")
+    command = "dmsmear -f {0} -b {1} -n {2} -d {3} -q".format(
+        rec_cfreq,
+        rec_bw,
+        rec_nchan,
+        dm
+    )
+    args = shlex.split(command)
+    result = subprocess.check_output(
+        args,
+        stderr=subprocess.STDOUT,
+        encoding="ascii"
+    )
 
     cand_band_smear = float(result.strip())
     log.info("cand_band_smear: {0}".format(cand_band_smear))
 
     cand_filter_time = (2 ** filter) * samp_time
-    log.info("Filter, cand_filter_time: {0}, {1}".format(filter,
-                                                             cand_filter_time))
+    log.info("Filter, cand_filter_time: {0}, {1}".format(
+        filter,
+        cand_filter_time
+        )
+    )
 
     cand_smearing = float(cand_band_smear) + float(cand_filter_time)
-    cand_start_time = cand_time - (0.5 * cand_smearing)
+    cand_start_time = cand_time - 0.5 * cand_smearing
     cand_tot_time = 2.0 * cand_smearing
 
     if length != 0:
@@ -383,7 +497,7 @@ zap_mode, nchan=0, nbin=0, length=0):
     if nbin > 1024:
         nbin = 1024
 
-    cmd = "dspsr " + fil_file + " -S " + str(cand_start_time) + \
+    command = "dspsr " + fil_file + " -S " + str(cand_start_time) + \
         " -b " + str(nbin) + \
         " -T " + str(cand_tot_time) + \
         " -c " + str(cand_tot_time) + \
@@ -392,17 +506,19 @@ zap_mode, nchan=0, nbin=0, length=0):
         " -cepoch start" + \
         " -q -Q"
 
-    log.info("dspsr cmd: {0}".format(cmd))
+    log.info("dspsr cmd: {0}".format(command))
 
     # create a temporary working directory
     workdir = tempfile.mkdtemp()
-    log.info("workdir: {0}".format(workdir))
+    log.info("Workdir: {0}".format(workdir))
 
-    args = shlex.split(cmd)
-    result = subprocess.check_output(args,
-                                     stderr=subprocess.STDOUT,
-                                     encoding="ascii",
-                                     cwd=workdir)
+    args = shlex.split(command)
+    result = subprocess.check_output(
+        args,
+        stderr=subprocess.STDOUT,
+        encoding="ascii",
+        cwd=workdir
+    )
 
     archive = result.split("seconds: ")[1]
     archive = archive.strip()
@@ -417,7 +533,7 @@ zap_mode, nchan=0, nbin=0, length=0):
 
     if nchan == 0:
         # determine number of channels based on SNR
-        nchan = int(round(math.pow(float(snr)/4.0,2)))
+        nchan = int(round(math.pow(float(snr) / 4.0, 2)))
     if nchan < 2:
         nchan = 2
 
@@ -456,7 +572,7 @@ zap_mode, nchan=0, nbin=0, length=0):
         "c{0:0>4}_{1}.png".format(cand_nr, os.path.basename(archive)[0:-3])
         )
 
-    cmd = """\
+    command = """\
 psrplot -p freq+
 -J {0} -j 'F {1:.0f}'
 -c above:l='{2}'
@@ -464,10 +580,17 @@ psrplot -p freq+
 -c above:r='{3}'
 -c x:unit=ms
 -c y:reverse=1
--D {4}/PNG {5}""".format(zap_file, nchan, info_str_l, info_str_r, outfile, archive)
+-D {4}/PNG {5}""".format(
+    zap_file,
+    nchan,
+    info_str_l,
+    info_str_r,
+    outfile,
+    archive
+    )
 
-    log.info("psrplot cmd: {0}".format(cmd))
-    args = shlex.split(cmd)
+    log.info("Psrplot cmd: {0}".format(command))
+    args = shlex.split(command)
     subprocess.check_call(args)
 
     # clean up
@@ -488,24 +611,7 @@ def main():
     setup_logging()
     log = logging.getLogger('hdpipe.candviewer')
 
-    # handle command line arguments
-    parser = argparse.ArgumentParser(description="View heimdall candidates.")
-    parser.add_argument("candfiles", type=str, nargs="+",
-                        help="Candidate files to process.")
-    parser.add_argument("-o", "--output", action="store_true", dest="output",
-                        default=False, help="Output plots to file rather than to screen.")
-    parser.add_argument("-z", "--zap_mode", dest="zap_mode", type=str,
-                        choices=[
-                            "None",
-                            "Lovell_20cm", "Lovell_80cm",
-                            "MeerKAT_20cm"
-                            ],
-                        default="None",
-                        help="Frequency zap mask mode to use (default: None).")
-    parser.add_argument("-n", "--nchan", type=int, dest="nchan",
-                        default=32, help="Scrunch to this many frequency channels (default: 32).")
-    parser.add_argument("--version", action="version", version=__version__)
-    args = parser.parse_args()
+    args = parse_args()
 
     # sanity check
     for item in args.candfiles:
@@ -520,15 +626,15 @@ def main():
     candfiles = np.sort(args.candfiles)
 
     data = None
-    i = 1
 
-    for item in candfiles:
+    for icand, item in enumerate(candfiles):
         print("Processing: {0}".format(item))
         part = load_data(item)
 
         part["cand_file"] = item
         part["fil_file"] = "{0}.fil".format(item[0:-5])
-        part["total_time"] =  part["time"] + (i - 1) * 60.0
+        # XXX: do not hardcode time here
+        part["total_time"] =  part["time"] + (icand - 1) * 60.0
 
         part = remove_bad_cands(part)
 
@@ -540,8 +646,6 @@ def main():
             data = np.copy(part)
         else:
             data = np.concatenate((data, part))
-
-        i += 1
 
     # remove all low-snr candidates and the ones that are really wide
     good = remove_bad_cands(data)
@@ -561,16 +665,27 @@ def main():
         and os.path.isfile(item["fil_file"]):
             pass
         else:
-            raise RuntimeError("Cand or fil file do not exist: {0}, {1}".format(item["cand_file"],
-                                                                                item["fil_file"]))
+            raise RuntimeError("Cand or fil file do not exist: {0}, {1}".format(
+                item["cand_file"],
+                item["fil_file"]
+                )
+            )
 
     cand_nr = 1
 
     for item in good:
         try:
-            plot_candidate_dspsr(item["fil_file"], item["cand_file"], cand_nr,
-                                 item["samp_nr"], item["filter"], item["dm"], item["snr"],
-                                 args.zap_mode, nchan=args.nchan)
+            plot_candidate_dspsr(
+                item["fil_file"],
+                item["cand_file"],
+                cand_nr,
+                item["samp_nr"],
+                item["filter"],
+                item["dm"],
+                item["snr"],
+                args.zap_mode,
+                nchan=args.nchan
+            )
         except subprocess.CalledProcessError as e:
             print("An error occurred: {0}".format(str(e)))
 
@@ -582,6 +697,5 @@ def main():
         plt.show()
 
 
-# if run directly
 if __name__ == "__main__":
     main()
